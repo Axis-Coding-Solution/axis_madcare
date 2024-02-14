ac3fbf84654ca02602014582ee8a09455bb08146
Afzal Dogar <dogard866@gmail.com>
2024-02-14 17:02:16 +0500

    home

--- src/app/(routes)/_components/main/our-services.tsx
+++ src/app/(routes)/_components/main/our-services.tsx
@@ -15,60 +15,28 @@ function OurServices() {
           <div className="grid grid-cols-4 gap-5 pl-24 pt-20">
             <div className="grid grid-cols-1  gap-5  cursor-pointer ">
               <div className=" shadow-xl rounded-md  bg-[#FFFFFF]  w-full   xl:h-[450px] xl:w-[330px] max-[320px]:pl-3 max-[375px]:pl-4 max-[425px]:pl-5 max-[280px]:pl-2 max-[768px]:pl-8 max-[1024px]:pl-9 lg:pl-3 xl:pl-0 pt-16 ">
-                <div className="px-10">
-                  <Image
-                    className="  "
-                    src="/icons/billing.png"
-                    alt="icon"
-                    width={100}
-                    height={50}
-                  />
-                  <h1 className="text-[#264d55]">Medical Billing and Coding</h1>
-                  <p>
-                    Medical billing and coding involves translating medical
-                    procedures and diagnoses into standardized codes for billing
-                    and insurance purposes.
-                  </p>
-                  <button className="text-white text-sm border-2 rounded-full h-[50px] px-6 mt-3 border-white hover:text-[#316069] hover:border-[#315a61] hover:bg-transparent bg-[#1a363b] transition-all duration-300">
-                    Request Demo
-                  </button>
+                <div className="">
+                  <div className="pl-28">
+                    <Image
+                      className=""
+                      src="/icons/billing.png"
+                      alt="icon"
+                      width={100}
+                      height={50}
+                    />
+                  </div>
+                  <div className="pl-10">
+                    <h5 className="mt-5 text-[#68959E] text-xl font-bold">Medical Billing and Coding</h5>
+                    <p className="pr-2 text-gray-400 text-xs mt-5">
+                      Medical billing and coding involves translating medical
+                      procedures and diagnoses into standardized codes for
+                      billing and insurance purposes.
+                    </p>
+                    <button className=" text-lg  rounded-full h-14 w-56 px-0 mt-12  font-bold    text-black  hover:bg-[#3791a4] ">
+                    View Detail
+                    </button>
+                  </div>
                 </div>
-
-                {/* <div>
-                  <div className="flex gap-1 mb-3 items-center">
-                    <IoMdArrowDropright className="text-[#949494]" />
-                    <p className="text-[#949494] group-hover:text-white">
-                      Vestibulum ut mauris ut massa
-                    </p>
-                  </div>
-                  <div className="flex gap-1 mb-3 items-center">
-                    <IoMdArrowDropright className="text-[#949494]" />
-                    <p className="text-[#949494] group-hover:text-white">
-                      Vestibulum ut mauris ut massa
-                    </p>
-                  </div>
-
-                  <div className="flex gap-1 mb-3 items-center">
-                    <IoMdArrowDropright className="text-[#949494]" />
-                    <p className="text-[#949494] group-hover:text-white">
-                      Vestibulum ut mauris ut massa
-                    </p>
-                  </div>
-
-                  <div className="flex gap-1 mb-3 items-center">
-                    <IoMdArrowDropright className="text-[#949494]" />
-                    <p className="text-[#949494] group-hover:text-white">
-                      Vestibulum ut mauris ut massa
-                    </p>
-                  </div>
-
-                  <div className="flex gap-1 mb-3 items-center">
-                    <IoMdArrowDropright className="text-[#949494]" />
-                    <p className="text-[#949494] group-hover:text-white">
-                      Vestibulum ut mauris ut massa
-                    </p>
-                  </div>
-                </div> */}
               </div>
             </div>
           </div>
