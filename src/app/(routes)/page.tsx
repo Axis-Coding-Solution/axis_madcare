ab3678f0f165dd4911e9fcd4b98bfc750fdcdcc6
hamza khalid <hamzakhalid.654@gmail.com>
2024-02-15 12:21:17 +0500

    aboutus

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "./_components/main/hero";
 import OurServices from "./_components/main/our-services";
 import EHR from "./_components/main/ehr";
+import Aboutus from "./_components/main/about-us"
 
 function MainPage() {
   return <div>
     <Hero/>
     <OurServices/>
     <EHR/>
+    <Aboutus/>
   </div>;
 }
 
