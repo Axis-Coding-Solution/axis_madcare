b23c5a211146f8d048f49679ef6fc207f14a8c39
hamza khalid <hamzakhalid.654@gmail.com>
2024-02-21 21:49:25 +0500

    services page

--- src/app/(routes)/ourservices/[slug]/page.tsx
+++ src/app/(routes)/ourservices/[slug]/page.tsx
@@ -1,161 +1,164 @@
 "use client";
-
-import React from "react";
-import { RxPencil2 } from "react-icons/rx";
-import { GiMagicPortal } from "react-icons/gi";
-import { AiFillInsurance } from "react-icons/ai";
-import { MdManageSearch } from "react-icons/md";
-import { PiRecycleDuotone } from "react-icons/pi";
-import Image from "next/image";
-import Header from "../ourservices/header";
-import Link from "next/link";
-import animation from "../../animation/Animation.json";
-import accountrceivable from "../../animation/accountrceivable.json";
-import Contracting from "../../animation/Contracting.json";
+import React, { useEffect, useState } from "react";
+import { useAppParams } from "@/utility/hooks";
+import DynamicHeader from "@/app/(routes)/_components/dynamic-page/dynamic-header";
+import R from "@/app/(routes)/_components/dynamic-page/dynamic-header";
+import Header from "../../_components/dynamic-page/header";
+import Info from "../../_components/dynamic-page/info";
+import Inter from "../../_components/dynamic-page/inter";
+import Contactus from "../../animation/Contactanimation.json";
+import emr from "../../animation/emr.json";
+import Billinganimation from "../../animation/Billinganimation.json";
 import Codinganimation from "../../animation/Codinganimation.json";
-import Accountanimation from "../../animation/Accountanimation.json";
-import Verificationanimation from "../../animation/Verificationanimation.json";
-import negotiation from "../../animation/negotiation.json";
-import Lottie from "lottie-react";
+import coding from "../../animation/Codinganimation.json";
+import account from "../../animation/Accountanimation.json";
+import rcm from "../../animation/rcm.json";
+import verify from "../../animation/Verificationanimation.json";
+import complete from "../../animation/Completeanimation.json";
+import RcmService from "../../_components/services/rcm-service";
+
+// import { useRouter } from 'next/navigation'
 
 const Page = () => {
-  function generateSlug(text) {
-    return text
-      .toLowerCase()
-      .replace(/ /g, "-")
-      .replace(/[^\w-]+/g, "");
-  }
+  const [service, setService] = useState<
+    | {
+        id: number;
+        icon: string;
+        title: string;
+        description: string;
+        image: string;
+      }
+    | null
+    | undefined
+  >(null);
 
-  const obj = [
+  const services = [
     {
       id: 1,
       // icon: "/icons/billing.png",
-      icon: animation,
+      icon: Contactus,
       title: "Medical Billing and Coding",
       slug: "medical-billing-and-coding",
       description:
         "Medical billing and coding involves translating medical procedures and diagnoses into standardized codes for billing and insurance purposes.",
-      image: "../../animation/Contactanimation.json",
-     },
+      image: coding,
+    },
     {
       id: 2,
-      icon:Accountanimation,
-      title: "Account Receivable",
+      icon: "/icons/account_receivable.png",
+      title: "Account Receivable Management",
       slug: "account-receivable-management",
-
       description:
         "Medical billing and coding involves translating medical procedures and diagnoses into standardized codes for billing and insurance purposes.,Account Receivable Management involves overseeing and optimizing the process of tracking and collecting payments owed to a business for goods or services provided.",
+      image: complete,
     },
     {
       id: 3,
-      icon:Contracting,
+      icon: "/icons/credentialing.png",
       title: "Credentialing & Contracting",
       slug: "credentialing-contracting",
-
       description:
         "Medical billing and coding involves translating medical procedures and diagnoses into standardized codes for billing and insurance purposes.,Account Receivable Management involves overseeing and optimizing the process of tracking and collecting payments owed to a business for goods or services provided.Refers to the process of verifying qualifications and negotiating agreements between healthcare providers and insurance companies or other payers to establish networks.",
-    },
+        image: account,
+      },
     {
       id: 4,
-      icon:negotiation,
+      icon: "/icons/networking.png",
       title: "Out Network Negotiation",
       slug: "out-network-negotiation",
 
       description:
         "Medical billing and coding involves translating medical procedures and diagnoses into standardized codes for billing and insurance purposes.,Account Receivable Management involves overseeing and optimizing the process of tracking and collecting payments owed to a business for goods or services provided.Out-Network Negotiation is the process of negotiating agreements and reimbursement rates between healthcare providers and insurance companies",
-    },
+        image: emr,
+      },
     {
       id: 5,
-      icon:Verificationanimation,
+      icon: "/icons/networking.png",
       title: "Eligibility and Benefit Verification",
       slug: "eligibility-and-benefit-verification",
+
       description:
         "Medical billing and coding involves translating medical procedures and diagnoses into standardized codes for billing and insurance purposes.,Account Receivable Management involves overseeing and optimizing the process of tracking and collecting payments owed to a business for goods or services provided.Eligibility and Benefit Verification is the process of confirming a patient's insurance coverage and determining the extent of their benefits for specific healthcare services",
+      image: verify,
     },
     {
       id: 6,
-      icon:animation,
+      icon: "/icons/credentialing.png",
       title: "Enrollment services",
       slug: "enrollment-services",
 
       description:
         "Medical billing and coding involves translating medical procedures and diagnoses into standardized codes for billing and insurance purposes.,Account Receivable Management involves overseeing and optimizing the process of tracking and collecting payments owed to a business for goods or services provided.Enrollment services encompass the procedures and support provided to individuals or entities seeking to enroll in various programs, memberships, or services",
-    },
+        image: account,
+      },
     {
       id: 7,
-      icon:animation,
+      icon: "/icons/account_receivable.png",
       title: "Complete RCM Services",
       slug: "complete-rcm-services",
 
       description:
         "Medical billing and coding involves translating medical procedures and diagnoses into standardized codes for billing and insurance purposes.Account Receivable Management involves overseeing and optimizing the process of tracking and collecting payments owed to a business for goods or services provided.Enrollment services encompass the procedures and support provided to individuals or entities seeking to enroll in various programs, memberships, or services",
+      image: Codinganimation,
     },
     {
       id: 8,
-      icon:animation,
+      icon: "/icons/billing.png",
       title: "EMR Applications and Support",
       slug: "emr-applications-and-support",
 
       description:
         "Medical billing and coding involves translating medical procedures and diagnoses into standardized codes for billing and insurance purposes.,Account Receivable Management involves overseeing and optimizing the process of tracking and collecting payments owed to a business for goods or services provided.Enrollment services encompass the procedures and support provided to individuals or entities seeking to enroll in various programs, memberships, or services",
-    },
+        image: rcm,
+      },
   ];
+
+  const params = useAppParams();
+
+  const { slug } = params;
+
+  // const parsedID = Number(slug);
+
+  useEffect(() => {
+    // const data = services.find((service) =>
+    //   service.id === parsedID;
+    // );
+
+    const data = services.find((s) => s.slug === slug);
+
+    console.log("data", data);
+    if (data)
+      // push back
+      setService(data);
+  }, []);
+
   return (
     <div>
-      <Header />
+      {slug === "complete-rcm-services" ? (
+        <RcmService />
+      ) : (
+        <>
+          <Header
+            title={service?.title}
+            description={service?.description}
+            image={service?.image}
+          />
+          <DynamicHeader
+            title={service?.title}
+            description={service?.description}
+            icon={service?.icon}
+          />
+          <Info title={service?.title} description={service?.description} />
 
-      <div className="bg-[url('/assests/images/doctors-bg.png')] bg-no-repeat bg-contain bg-top">
-        <div className="bg-[url('/assests/images/bg-departments.png')] bg-no-repeat h-[15vh]  bg-top md:px-40 mt-10">
-          <div className="  pt-7 ">
-            <h1 className="text-[#3f7884] text-center font-bold  tracking-tighter   text-[40px] leading-[24px] not-italic">
-              OUR SERVICES
-            </h1>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 mb-10 sm:grid-cols-2 lg:grid-cols-4 gap-8 mx-5 lg:mx-24 sm:pt-12  ">
-          {obj.map((service, index) => {
-            const slugName = generateSlug(service.title);
-            return (
-              <div key={service.id} className="cursor-pointer ">
-                <Link
-                  // href={`/${slugName}/${service.id}`}
-                  href={`/servicess/${service.slug}`}
-                >
-                  <div className=" group shadow-xl border   bg-[#FFFFFF]  hover:bg-[#3f7884] rounded-lg transition-all duration-300 h-[220px] pb-4">
-                    <div className="flex flex-col justify-between items-center">
-                      <div className="group flex flex-col gap-5 h-[200px] items-center text-center relative">
-                        {/* <Image
-                          src={service.icon}
-                          alt="icon"
-                          objectFit="cover"
-                          width={100}
-                          height={40}
-                          className="text-black"
-                        /> */}
-
-                        <div
-                          style={{ width: "40%", height: "10%" }}
-                          className=" top-0"
-                        >
-                          <Lottie
-                            animationData={service.icon}
-                            loop={true} // Set to true if you want the animation to loop
-                            autoplay={true} // Set to true if you want the animation to play automatically
-                          />
-                          </div>
-
-                        <div className=" absolute bottom-0 mb-10 group-hover:text-white"><h5 className="">{service.title}</h5></div>
-                      </div>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+          <Inter
+            title={service?.title}
+            description={service?.description}
+            icon={service?.icon}
+          />
+        </>
+      )}
     </div>
   );
 };
+
 export default Page;
--- src/app/(routes)/ourservices/page.tsx
+++ src/app/(routes)/ourservices/page.tsx
@@ -1,12 +1,33 @@
 "use client";
-import Image from "next/image";
-import { fjalla } from "@/utility/font";
-import Link from "next/link";
-import Billinganimation from "../../animation/Billinganimation.json";
-import Lottie from "lottie-react";
 
-function OurServices() {
-  const services = [
+import React from "react";
+import { RxPencil2 } from "react-icons/rx";
+import { GiMagicPortal } from "react-icons/gi";
+import { AiFillInsurance } from "react-icons/ai";
+import { MdManageSearch } from "react-icons/md";
+import { PiRecycleDuotone } from "react-icons/pi";
+import Header from "../_components/ourservices/header";
+import Link from "next/link";
+import Contracting from "../animation/Contracting.json";
+import Codinganimation from "../animation/Codinganimation.json";
+import Accountanimation from "../animation/Accountanimation.json";
+import Verificationanimation from "../animation/Verificationanimation.json";
+import Enrollment from "../animation/Enrollment.json";
+import emr from "../animation/emr.json";
+import rcm from "../animation/rcm.json";
+import outnetwork from "../animation/outnetwork.json";
+import Lottie from "lottie-react";
+import Image from "next/image";
+
+const Page = () => {
+  function generateSlug(text: string) {
+    return text
+      .toLowerCase()
+      .replace(/ /g, "-")
+      .replace(/[^\w-]+/g, "");
+  }
+
+  const obj = [
     {
       id: 1,
       icon: "/icons/billing.png",
@@ -80,91 +101,63 @@ function OurServices() {
     },
   ];
   return (
-    <>
-      <div className=" pt-5 mt-60 sm:mt-10">
-        <div className="bg-[url('/assests/images/bg-departments.png')] bg-no-repeat h-[30vh]  bg-top md:px-40">
+    <div>
+      <Header />
+
+      <div className="bg-[url('/assests/images/doctors-bg.png')] bg-no-repeat bg-contain bg-top">
+        <div className="bg-[url('/assests/images/bg-departments.png')] bg-no-repeat h-[15vh]  bg-top md:px-40 mt-10">
           <div className="  pt-7 ">
             <h1 className="text-[#3f7884] text-center font-bold  tracking-tighter   text-[40px] leading-[24px] not-italic">
               OUR SERVICES
             </h1>
           </div>
         </div>
-        <div className="bg-[url('/assests/images/doctors-bg.png')] bg-no-repeat bg-contain bg-top">
-          <div className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-4 gap-8 mx-5 lg:mx-24 sm:pt-12 xl:pt-0  ">
-            {services.map((service, index) => (
-              <div
-                key={service.id}
-                // className="cursor-pointer"
-                onClick={() => console.log(index + 1)}
-              >
-                {/* {console.log("",index)} */}
 
-                {/* <Link
-                  href={`ourservices/${service.slug}`}
-                  // href="/dynamicpage/${service.id}"
-                > */}
-                  <div className=" shadow-xl    bg-[#FFFFFF]  rounded-lg     h-[520px]    pt-12 pb-4">
-                    <div className="flex flex-col justify-between items-center  ">
-                      <div className="flex flex-col gap-5 h-[200px] items-center text-center  ">
-                        <Image
+        <div className="grid grid-cols-1 mb-10 sm:grid-cols-2 lg:grid-cols-4 gap-8 mx-5 lg:mx-24 sm:pt-12  ">
+          {obj.map((service, index) => {
+            const slugName = generateSlug(service.title);
+            return (
+              <div key={service.id} className="cursor-pointer ">
+                <Link
+                  // href={`/${slugName}/${service.id}`}
+                  href={`/ourservices/${service.slug}`}
+                >
+                  <div className=" group shadow-xl border   bg-[#FFFFFF]  hover:bg-[#3f7884] rounded-lg transition-all duration-300 h-[220px] pb-4">
+                    <div className="flex flex-col justify-between items-center">
+                    <div className="group flex flex-col gap-5 h-[200px] items-center text-center mt-10">
+                      <Image
                           src={service.icon}
                           alt="icon"
                           objectFit="cover"
                           width={100}
                           height={40}
+                          className="text-black"
                         />
-                        <h5
-                          className={`${fjalla.className}  text-[#3f7884]   font-bold  text-[20px]`}
-                        >
-                          {service.title}
-                        </h5>
-                      </div>
-                      <div className="h-[100px]">
-                        <p className="pr-2 text-[#7b7a8b] text-center font-normal text-[11px]  px-2  ">
-                          {service.description}
-                        </p>
-                      </div>
-                      <div className="">
-                        <button className=" text-[16px]   rounded-full h-14 w-44 px-0 mt-12 border-[1px] border-[#3791a4]  font-bold  cursor-pointer   text-[#68959E] hover:text-[#fffefe] hover:bg-[#3F7884] transition-all duration-300 ">
-                          View Detail
-                        </button>
-                      </div>
-                    </div>
-                    {/* <div className="">
-                    <div className="flex flex-col items-center justify-between  ">
-                      <Image
-                        src={service.icon}
-                        alt="icon"
-                        objectFit="cover"
-                        width={100}
-                        height={40}
-                      />
-                    </div>
-                    <div className=" text-center  ">
-                      <h5
-                        className={`${fjalla.className} mt-10 text-[#3f7884]   font-bold  text-[20px]`}
+
+                      {/* <div
+                        style={{ width: "40%", height: "10%" }}
+                        className=" top-0"
                       >
-                        {service.title}
-                      </h5>
-                      <p className="pr-2 text-[#7b7a8b] font-normal text-[12px]  px-2     mt-5 ">
-                        {service.description}
-                      </p>
-                      <div className="">
-                        <button className=" text-[16px]   rounded-full h-14 w-44 px-0 mt-12 border-[1px] border-[#3791a4]  font-bold  cursor-pointer   text-[#68959E] hover:text-[#fffefe] hover:bg-[#3F7884] transition-all duration-300 ">
-                          View Detail
-                        </button>
+                        <Lottie
+                          animationData={service.image}
+                          loop={true} // Set to true if you want the animation to loop
+                          autoplay={true} // Set to true if you want the animation to play automatically
+                        />
+                      </div> */}
+
+                      <div className="  mb-10 group-hover:text-white">
+                        <h5 className="">{service.title}</h5>
                       </div>
                     </div>
-                  </div> */}
+                    </div>
                   </div>
-                {/* </Link> */}
+                </Link>
               </div>
-            ))}
-          </div>
+            );
+          })}
         </div>
       </div>
-    </>
+    </div>
   );
-}
-
-export default OurServices;
+};
+export default Page;
